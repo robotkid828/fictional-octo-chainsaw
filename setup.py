2dafe40276b42f0e3011f151f31619026d499885
robotkid828 <113220796+robotkid828@users.noreply.github.com>
2022-11-16 15:59:49 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
-from main import __version__
+from mainthing import __version__
 
 setup(
-    name='main',
+    name='mainthing',
     version=__version__,
 
     url='https://github.com/robotkid828/fictional-octo-chainsaw',
     author='robotkid828',
     author_email='',
 
-    py_modules=['main'],
+    py_modules=['mainthing'],
 )
