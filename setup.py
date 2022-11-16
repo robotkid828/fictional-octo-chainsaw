664faa7e8b5544313837ef85f695cc35d27f4cdc
robotkid828 <113220796+robotkid828@users.noreply.github.com>
2022-11-16 15:52:11 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+from main import __version__
+
+setup(
+    name='main',
+    version=__version__,
+
+    url='https://github.com/robotkid828/fictional-octo-chainsaw',
+    author='robotkid828',
+    author_email='',
+
+    py_modules=['main'],
+)
